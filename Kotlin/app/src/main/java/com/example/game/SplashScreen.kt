2572f9c47de4a4558615db9df7134bebb39eb4dd
elvira.yuhnevich <elvira.yuhnevich@gmail.com>
2022-04-05 12:02:17 +0200

    Splash Screen

---
+++ Kotlin/app/src/main/java/com/example/game/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.example.game
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val thread = Thread(){
+            run{
+                Thread.sleep(3000)
+            }
+            runOnUiThread(){
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        thread.start()
+    }
+}
